3e84769555236ea7be6845406f57e3d6df49a7ce
ekan07 <aniekanbon@gmail.com>
2023-01-18 00:20:06 +0100

    Add file: 100-find, 101-students.py, 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""All students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    top_stds = mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
+    return top_stds
